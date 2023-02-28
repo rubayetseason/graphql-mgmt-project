dbe052e0f928290367d7f321c7a0ec60fed8c7af
rubayet-islam <season.ss2001@gmail.com>
2023-02-28 14:25:23 +0600

    connected with mongoose and created client, project mongoose model schema

--- graphql-crud-server/index.js
+++ graphql-crud-server/index.js
@@ -5,10 +5,18 @@ const schema = require("./Schema/schema");
 const cors = require("cors");
 app.use(cors());
 const { graphqlHTTP } = require("express-graphql");
+require("dotenv").config();
+const connectDB = require("./config/db");
 
-app.use("/graphql", graphqlHTTP({
+//connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true,
-}));
+  })
+);
 
 app.listen(PORT, console.log(`listening to port ${PORT}`));
